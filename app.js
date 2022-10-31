f1ec88c2f8b6979effd62dc50874c62f75a141be
ruthdorgbaa <ruthdorgbaa@gmail.com>
2022-10-31 09:50:57 +0100

    fourth commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// const menu = document.querySelector(".menu");
+// const menuItem = document.querySelectorAll(".items");
+// const hamburger = document.querySelector(".hamburger");
+// const closeIcon = document.querySelector(".closeIcon");
+// const menuIcon = document.querySelector(".menuIcon");
+
+// function toggleMenu() {
+//     if (menu.classList.contains("showMenu")) {
+//         menu.classList.remove("showMenu");
+//         closeIcon.getElementsByClassName.display = "none";
+//         menuIcon.getElementsByClassName.display = "block";
+//         console.log("here")
+//     } else {
+//         menu.classList.add("showMenu");
+//         closeIcon.style.display = "block";
+//         menuIcon.style.display = "none";
+//     }
+// }
+// hamburger.addEventListener("click", toggleMenu);
