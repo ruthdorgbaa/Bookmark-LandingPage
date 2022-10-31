fbac5998fdde8e33aff21e5208f46f12e4af5028
okikechinonso <chinonsookike@gmail.com>
2022-10-31 15:50:37 +0100

    first commit

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const hamburger = document.querySelector(".hamburger");
-const menuNav = document.querySelector(".mobile-nav");
-const closeIcon = document.querySelector(".closeIcon");
+// const hamburger = document.querySelector(".hamburger");
+// const menuNav = document.querySelector(".mobile-nav");
+// const closeIcon = document.querySelector(".closeIcon");
 
-let isVisible = false
+// let isVisible = false
 
-function toggleMenu() {
-    if (!isVisible) {
-        menuNav.style.cssText = "visibility: visible !important;";
-        isVisible = true;
-    }else{
-        menuNav.style.cssText = "visibility: hidden !important;";
-        isVisible = false
-    }  
-    console.log("here")
-}
-hamburger.addEventListener("click", toggleMenu);
-closeIcon.addEventListener("click", toggleMenu);
+// function toggleMenu() {
+//     if (!isVisible) {
+//         menuNav.style.cssText = "visibility: visible !important;";
+//         isVisible = true;
+//     }else{
+//         menuNav.style.cssText = "visibility: hidden !important;";
+//         isVisible = false
+//     }  
+//     console.log("here")
+// }
+// hamburger.addEventListener("click", toggleMenu);
+// closeIcon.addEventListener("click", toggleMenu);
 
 
 
