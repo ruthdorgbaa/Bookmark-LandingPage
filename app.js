9cc77f335e39bc836c1ca128cbc264f77361ae66
okikechinonso <chinonsookike@gmail.com>
2022-10-31 13:04:37 +0100

    new commit

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const menu = document.querySelector(".hamburger");
-const menuIcon = document.querySelector(".mobile-nav");
+const hamburger = document.querySelector(".hamburger");
+const menuNav = document.querySelector(".mobile-nav");
 const closeIcon = document.querySelector(".closeIcon");
 
 let isVisible = false
 
 function toggleMenu() {
     if (!isVisible) {
-        menuIcon.style.cssText = "visibility: visible !important;";
+        menuNav.style.cssText = "visibility: visible !important;";
         isVisible = true;
     }else{
-        menuIcon.style.cssText = "visibility: hidden !important;";
+        menuNav.style.cssText = "visibility: hidden !important;";
         isVisible = false
-    }
-    
+    }  
     console.log("here")
 }
-menu.addEventListener("click", toggleMenu);
+hamburger.addEventListener("click", toggleMenu);
 closeIcon.addEventListener("click", toggleMenu);
 
 
